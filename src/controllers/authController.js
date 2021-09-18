254126714c495397f5b6bac7c86324d42cf471dd
PabloSSena <pablosilvasena@gmail.com>
2021-09-18 17:52:34 -0300

    Adicionando geraçao de token de autenticaçao

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const User = require('../models/user');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const authConfig = require('../config/auth.json')
 
 router.post('/register', async (req,res) => {
     const {email} = req.body;
@@ -19,5 +22,25 @@ router.post('/register', async (req,res) => {
         return res.status(400).send({ error: 'Registration failed' });
     }
 });
+
+router.post('/authenticate', async (req,res) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email }).select('+password')
+
+    if(!user)
+        return res.status(400).send({error:'User not found'});
+
+    if(!await bcrypt.compare(password, user.password))
+        return res.status(400).send({error:'Invalid password'});
+    
+    user.password = undefined;
+
+    const token = jwt.sign({id: user.id}, authConfig.secret, {
+        expiresIn:90000, 
+    } )
+
+    res.send({user, token});
+})
 
 module.exports = app => app.use('/api', router)
